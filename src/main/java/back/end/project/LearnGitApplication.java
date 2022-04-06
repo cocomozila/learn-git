efc5d7dfbdf6705b56ef0badc5a65480fc4b460d
cocomozila <joseyun94@naver.com>
2022-04-06 15:07:21 +0900

    init

---
+++ src/main/java/back/end/project/LearnGitApplication.java
@@ -0,0 +1,13 @@
+package back.end.project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearnGitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LearnGitApplication.class, args);
+	}
+
+}
---
+++ src/main/java/back/end/project/ServletInitializer.java
@@ -0,0 +1,13 @@
+package back.end.project;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(LearnGitApplication.class);
+	}
+
+}
